1cbb6a2e1c0d162b8f9920f66f550566e1e2b2be
Anders Musikka <anders@andersmusikka.se>
2021-08-12 11:15:06 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,15 @@
 #![feature(int_log)]
 #![feature(stdsimd)]
+#![feature(bench_black_box)]
+#![feature(test)]
 
 use std::ops::{Sub, Add};
 use std::collections::HashMap;
 use std::hash::Hash;
-use std::arch::x86_64::{_mm_broadcastb_epi8, __m128i, _mm_set1_epi8, _mm_cvtsi128_si64x, _mm_maskz_broadcastb_epi8};
+use std::arch::x86_64::{__m128i, _mm_set1_epi8, _mm_cvtsi128_si64x, _mm_and_si128, _mm_set1_epi64x};
+
+#[cfg(all(target_feature = "avx512vl", target_feature="avx512bw"))]
+use std::arch::x86_64::{_mm_broadcastb_epi8, _mm_maskz_broadcastb_epi8};
 
 #[derive(Clone,Copy,PartialEq,Debug,Eq,Hash)]
 pub struct Pos {
@@ -38,14 +43,15 @@ impl BB {
             }
         }
     }
+    #[inline(always)]
     pub fn overlaps(self, other: BB) -> bool {
         if  self.top_left.x >= other.bottom_right.x ||
             self.top_left.y >= other.bottom_right.y ||
-            self.bottom_right.x <= other.bottom_right.x ||
-            self.bottom_right.y <= other.bottom_right.y {
-            true
-        } else {
+            self.bottom_right.x <= other.top_left.x ||
+            self.bottom_right.y <= other.top_left.y {
             false
+        } else {
+            true
         }
     }
 }
@@ -110,7 +116,7 @@ struct TreeNode<T> {
     node_payload: Vec<T>,
     node_children: Vec<u32>,
     next_free: Option<u32>, //If TreeNode is part of free-list, this is the next free
-    parent: u32, //0 means no parent,
+    parent: u32, //The root has parent id 0. It doesn't really have a parent of its own, but it has 0 here.
     node_place: u32,
 }
 
@@ -119,6 +125,8 @@ pub struct QuadBTree<T:TreeNodeItem> {
     first_free: Option<u32>, //If there are free nodes, this is the first free
     items: HashMap<T::Key, u32>, //map item key to tree node
 }
+
+
 
 impl<T:TreeNodeItem> QuadBTree<T> {
 
@@ -155,25 +163,56 @@ impl<T:TreeNodeItem> QuadBTree<T> {
     }
 
     fn remove_node_if_unused(&mut self, node_index: usize) {
+        if node_index == 0 {
+            return; //Root node is never removed.
+        }
         let node : &mut TreeNode<T> = &mut self.tree[node_index];
         let parent_index = node.parent as usize;
-        if node.node_payload.is_empty() == false || node.node_children.is_empty() {
+        if node.node_payload.is_empty() == false || node.node_children.is_empty()==false {
             return;
         }
 
 
-        if parent_index != 0 {
+        debug_assert_eq!(node.node_bitmap,0);
+        node.next_free = self.first_free;
+        self.first_free = Some(node_index as u32);
+        {
             let node_node_place = node.node_place;
             let parent_node:&mut TreeNode<T> = &mut self.tree[parent_index];
-            let place = get_set_bits_below(parent_node.node_bitmap, node_node_place as u64);
-            parent_node.node_children.remove(place as usize);
-            parent_node.node_bitmap &= !(1u64<<place);
+            let place_index = get_set_bits_below(parent_node.node_bitmap, node_node_place as u64);
+            parent_node.node_children.remove(place_index as usize);
+            parent_node.node_bitmap &= !(1u64<<node_node_place);
+            debug_assert_eq!(parent_node.node_bitmap.count_ones() as usize, parent_node.node_children.len());
             self.remove_node_if_unused(parent_index)
         }
     }
 
-    #[target_feature(enable = "avx2")]
-    #[cfg(target_arch = "x86_64")]
+    #[cfg(all(target_feature = "avx512vl", target_feature="avx512bw"))]
+    #[inline(always)]
+    fn spread_op(x1:i32,y1:i32,x2:i32, y2:i32) -> u64 {
+        let each_row = ((2<<(x2 as u8))-1) & ! ((1<<x1)-1);
+        let rows_mask = ((2<<(y2 as u8))-1) & ! ((1<<y1)-1);
+        unsafe {
+            let xtemp =  _mm_set1_epi8(each_row);
+            let x = _mm_maskz_broadcastb_epi8(rows_mask, xtemp);
+            let x = _mm_cvtsi128_si64x(x) as u64;
+            x
+        }
+    }
+
+    #[cfg(not(all(target_feature = "avx512vl", target_feature="avx512bw")))]
+    #[inline(always)]
+    fn spread_op(x1:i32,y1:i32,x2:i32, y2:i32) -> u64 {
+        let each_row = ((2u8<<(x2 as u8)).wrapping_sub(1u8)) & ! ((1<<x1)-1);
+        let rows_mask = ((256u64<<(y2 as u64*8)).wrapping_sub(1)) & ! ((1u64<<(y1*8))-1);
+        unsafe {
+            let mm_all_rows = _mm_set1_epi8(each_row as i8);
+            let mm_rows_mask: __m128i = _mm_set1_epi64x(rows_mask as i64);
+            let result = _mm_and_si128(mm_all_rows, mm_rows_mask);
+            let x = _mm_cvtsi128_si64x(result) as u64;
+            return x;
+        }
+    }
     fn query_impl<'a,F:FnMut(&'a T)>(&'a self, node_index: usize, bb: BB, f:&mut F) {
         let node = &self.tree[node_index];
         for payload_item in &node.node_payload {
@@ -182,38 +221,34 @@ impl<T:TreeNodeItem> QuadBTree<T> {
             }
         }
 
+        if node.node_bitmap==0 {
+            return;
+        }
+
         let off : BB = bb - node.bb.top_left;
         let x1 = (off.top_left.x>>node.sub_cell_shift).clamp(0,7);
         let x2 = (off.bottom_right.x>>node.sub_cell_shift).clamp(0,7);
         let y1 = (off.top_left.y>>node.sub_cell_shift).clamp(0,7);
         let y2 = (off.bottom_right.y>>node.sub_cell_shift).clamp(0,7);
 
-        let each_row = ((2<<(x2 as u8))-1) & ! ((1<<x1)-1);
-        let rows_mask = ((2<<(y2 as u8))-1) & ! ((1<<y1)-1);
-
-
-        unsafe {
-            let xtemp =  _mm_set1_epi8(each_row);
-
-            compile_error!("Make something smart")
-            let x = _mm_maskz_broadcastb_epi8(rows_mask, xtemp);
-
-            let x = _mm_cvtsi128_si64x(x) as u64;
-
-            for y in y1..=y2 {
-                for x in x1..=x2 {
-                }
-            }
-
+        let bb_mask = Self::spread_op(x1,y1,x2,y2);
+        let mut child_mask:u64 = node.node_bitmap&bb_mask;
+        while child_mask!=0 {
+            let cur_child = child_mask.trailing_zeros() as u64;
+            let cur_child_mask = 1<<cur_child;
+            child_mask &= !cur_child_mask;
+            let child_place = ((cur_child_mask-1)&node.node_bitmap).count_ones();
+            let child_node_index = node.node_children[child_place as usize];
+            self.query_impl(child_node_index as usize, bb, &mut *f);
         }
-
-
     }
+    #[inline(always)]
     pub fn query(&self, bb:BB) -> Vec<&T> {
         let mut ret = vec![];
         self.query_impl(0, bb, &mut |item|ret.push(item));
         ret
     }
+    #[inline(always)]
     pub fn query_fn<F:FnMut(&T)>(&self, bb: BB, mut f:F) {
         self.query_impl(0,bb,&mut f)
     }
@@ -223,11 +258,11 @@ impl<T:TreeNodeItem> QuadBTree<T> {
     /// Return true if the item was found and removed,
     /// false otherwise.
     pub fn remove(&mut self, key: T::Key) -> bool {
-        if let Some(index) = self.items.remove(&key) {
-            let node : &mut TreeNode<T> = &mut self.tree[index as usize];
-            if let Some(index) = node.node_payload.iter().position(|x|x.get_key()==key) {
-                node.node_payload.swap_remove(index);
-                self.remove_node_if_unused(index);
+        if let Some(node_index) = self.items.remove(&key) {
+            let node : &mut TreeNode<T> = &mut self.tree[node_index as usize];
+            if let Some(placement_index) = node.node_payload.iter().position(|x|x.get_key()==key) {
+                node.node_payload.swap_remove(placement_index);
+                self.remove_node_if_unused(node_index as usize);
                 true
             } else {
                 false //We can only get here if some of the user's trait-implementations panic
@@ -248,17 +283,18 @@ impl<T:TreeNodeItem> QuadBTree<T> {
 
         let item_bb:BB = item.get_bb();
         let off : BB = item_bb - node.bb.top_left;
-        let x1 = off.top_left.x>>node.sub_cell_shift;
-        let x2 = off.bottom_right.x>>node.sub_cell_shift;
-        let y1 = off.top_left.y>>node.sub_cell_shift;
-        let y2 = off.bottom_right.y>>node.sub_cell_shift;
+        let x1 = (off.top_left.x>>node.sub_cell_shift).clamp(0,7);
+        let x2 = (off.bottom_right.x>>node.sub_cell_shift).clamp(0,7);
+        let y1 = (off.top_left.y>>node.sub_cell_shift).clamp(0,7);
+        let y2 = (off.bottom_right.y>>node.sub_cell_shift).clamp(0,7);
 
         if x1==x2 && y1==y2 && node.sub_cell_size >= 8 {
-            let bitmap_index = x1 + y1<<3;
+            let bitmap_index = x1 + (y1<<3);
             if node.node_bitmap & (1<<(bitmap_index as usize)) != 0 {
                 // Child node exists
                 let insertion_place = get_set_bits_below(node.node_bitmap, bitmap_index as u64);
                 let child_node_index = node.node_children[insertion_place as usize] as usize;
+                debug_assert_eq!(node.node_bitmap.count_ones() as usize, node.node_children.len());
                 self.insert_impl(item, child_node_index);
 
             } else {
@@ -266,26 +302,35 @@ impl<T:TreeNodeItem> QuadBTree<T> {
                 let insertion_place = get_set_bits_below(node.node_bitmap, bitmap_index as u64);
                 node.node_bitmap |= 1<<(bitmap_index as usize);
 
+                let top_left = Pos {
+                    x: x1<<node.sub_cell_shift,
+                    y: y1<<node.sub_cell_shift,
+                };                let bb = BB {
+                    top_left,
+                    bottom_right: top_left + Pos{x:node.sub_cell_size-1,y:node.sub_cell_size-1}
+                };
+                let sub_cell_size = node.sub_cell_size/8;
+                let sub_cell_shift = node.sub_cell_shift-3;
                 let new_node;
                 let new_child_node;
                 if let Some(free) = self.first_free {
                     new_child_node = free as usize;
                     node.node_children.insert(insertion_place as usize, new_child_node as u32);
+                    debug_assert_eq!(node.node_bitmap.count_ones() as usize, node.node_children.len());
                     new_node = &mut self.tree[free as usize];
                     self.first_free = new_node.next_free;
+                    new_node.bb = bb;
+                    new_node.sub_cell_size = sub_cell_size;
+                    new_node.sub_cell_shift = sub_cell_shift;
+                    debug_assert_eq!(new_node.node_bitmap,0);
+                    debug_assert_eq!(new_node.node_payload.len(),0);
+                    debug_assert_eq!(new_node.node_children.len(),0);
+                    new_node.parent = node_index as u32;
+                    new_node.node_place = bitmap_index as u32;
                 } else {
                     new_child_node = self_tree_len;
                     node.node_children.insert(insertion_place as usize, new_child_node as u32);
-                    let top_left = Pos {
-                        x: x1<<node.sub_cell_shift,
-                        y: y1<<node.sub_cell_shift,
-                    };
-                    let bb = BB {
-                        top_left,
-                        bottom_right: top_left + Pos{x:node.sub_cell_size-1,y:node.sub_cell_size-1}
-                    };
-                    let sub_cell_size = node.sub_cell_size/8;
-                    let sub_cell_shift = node.sub_cell_shift-3;
+                    debug_assert_eq!(node.node_bitmap.count_ones() as usize, node.node_children.len());
                     self.tree.push(TreeNode {
                         bb,
                         sub_cell_size,
@@ -313,12 +358,27 @@ impl<T:TreeNodeItem> QuadBTree<T> {
 #[cfg(test)]
 mod tests {
     use crate::{QuadBTree, BB, TreeNodeItem};
+    use rand::Rng;
+    use rand::prelude::*;
+    use rand_pcg::Pcg64;
+    use std::collections::HashSet;
+    use test::Bencher;
+    use std::hint::black_box;
+    use std::time::Instant;
 
-    #[derive(Clone,Copy,PartialEq,Debug)]
+    extern crate test;
+
+    pub(crate) fn for_test_get_first_free<T:TreeNodeItem>(tree:&QuadBTree<T>) -> Option<u32> {
+        tree.first_free
+    }
+
+
+    #[derive(Clone,Copy,PartialEq,Debug,Eq,Hash)]
     pub struct TestItem {
         id: u32,
         pos: BB,
     }
+
 
     impl TreeNodeItem for TestItem {
         type Key = u32;
@@ -334,19 +394,164 @@ mod tests {
 
 
 
+    fn random_insert_and_query_test(seed:u64, operations: u32, deletes: bool, gridsize: i32, max_size: Option<i32>) {
+
+        let max_size = max_size.unwrap_or(i32::MAX);
+        let mut rng = Pcg64::seed_from_u64(seed);
+
+        let mut g = QuadBTree::new(gridsize);
+        let mut all_inserted:Vec<TestItem> = Vec::new();
+        for i in 0..operations {
+            if deletes && rng.gen_bool(0.5) && all_inserted.is_empty()==false {
+                let to_remove_index = rng.gen_range(0..all_inserted.len());
+                let to_remove_key = all_inserted[to_remove_index].id;
+                g.remove(to_remove_key);
+                all_inserted.swap_remove(to_remove_index);
+            } else {
+                let x1 = rng.gen_range(0..gridsize);
+                let y1 = rng.gen_range(0..gridsize);
+                let x2 = rng.gen_range(x1..(x1+max_size).min(gridsize));
+                let y2 = rng.gen_range(y1..(y1+max_size).min(gridsize));
+                let test_item = TestItem {
+                    id: i,
+                    pos: BB::new(x1,y1,x2,y2)
+                };
+                //println!("Inserting {:?}", test_item);
+
+                g.insert(test_item);
+                all_inserted.push(test_item);
+            }
+        }
+        println!("After {} operations, has {} tree nodes.", operations, g.tree.len());
+        for _i in 0..10 {
+            let x1 = rng.gen_range(0..gridsize);
+            let y1 = rng.gen_range(0..gridsize);
+            let x2 = rng.gen_range(x1..gridsize);
+            let y2 = rng.gen_range(y1..gridsize);
+            let pos= BB::new(x1,y1,x2,y2);
+            //println!("Querying {:?}", pos);
+            let g_hit :HashSet<TestItem>= g.query(pos).iter().cloned().cloned().collect();
+            let correct_hits:HashSet<TestItem> = all_inserted.iter().filter(|x|x.pos.overlaps(pos)).cloned().collect();
+            assert_eq!(g_hit,correct_hits);
+
+        }
+
+
+    }
     #[test]
-    fn basic_test1() {
+    fn exhaustive_insert_query_single_insert_test() {
+        for seed in 0..100000 {
+            //println!("Testing seed {}", seed);
+            random_insert_and_query_test(seed,1,false,1024, None);
+        }
+    }
+    #[test]
+    fn exhaustive_insert_remove_query_single_op_test() {
+        for seed in 0..100000 {
+            //println!("Testing seed {}", seed);
+            random_insert_and_query_test(seed,2,true,1024, None);
+        }
+    }
+    #[test]
+    fn exhaustive_insert_remove_query_test() {
+        for seed in 0..100000 {
+            //println!("Testing seed {}", seed);
+            random_insert_and_query_test(seed,100,true,1024, None);
+        }
+    }
+    #[test]
+    fn exhaustive_insert_query_test() {
+        for seed in 0..100000 {
+            //println!("Testing seed {}", seed);
+            random_insert_and_query_test(seed,10,false,1024, None);
+        }
+    }
+    #[test]
+    fn exhaustive_insert_query_test_large_grid() {
+        for seed in 0..100000 {
+            //println!("Testing seed {}", seed);
+
+            random_insert_and_query_test(seed,50,true,65536*1024, Some(512));
+        }
+    }
+    #[test]
+    fn exhaustive_insert_test_seed118() {
+        random_insert_and_query_test(118,10,false,1024, None);
+    }
+    #[test]
+    fn exhaustive_insert_remove_test_seed9() {
+        random_insert_and_query_test(9,2,true,1024, None);
+    }
+    #[test]
+    fn basic_remove_test1() {
+        let mut g = QuadBTree::new(256);
+        let test_item = TestItem {
+            id: 42,
+            pos: BB::new(10, 10, 10, 10)
+        };
+        g.insert(test_item);
+        assert_eq!(g.query(BB::new(9,9,11,11)), vec![&test_item]);
+        g.remove(42);
+
+        assert_eq!(g.query(BB::new(9,9,11,11)), Vec::<&TestItem>::new());
+
+        let first_free = for_test_get_first_free(&g);
+        assert_eq!(first_free, Some(1));
+    }
+    #[test]
+    fn basic_insert_test1() {
         let mut q = QuadBTree::new(256);
+        assert_eq!(q.query(BB::new(0,0,255,255)), Vec::<&TestItem>::new());
+
         let test_item = TestItem {
             id: 42,
             pos: BB::new(10,10,15,15)
         };
         q.insert(test_item);
 
-        assert_eq!(q.query(BB::new(9,9,16,16)), vec![&test_item]);
         assert_eq!(q.query(BB::new(9,9,11,11)), vec![&test_item]);
+
+        assert_eq!(q.query(BB::new(9,9,16,16)), vec![&test_item]);
         assert_eq!(q.query(BB::new(2,2,4,4)), Vec::<&TestItem>::new());
         q.remove(42);
         assert_eq!(q.query(BB::new(9,9,16,16)), Vec::<&TestItem>::new());
+    }
+
+    #[bench]
+    fn benchmark_random_queries(b: &mut Bencher) {
+        let mut rng = Pcg64::seed_from_u64(42);
+        let gridsize = 8192;
+        let max_size = 1024;
+        compile_error!("Figure out why this isn't faster!");
+        let mut g = QuadBTree::new(gridsize);
+        for i in 0..1_000 {
+            let x1 = rng.gen_range(0..gridsize);
+            let y1 = rng.gen_range(0..gridsize);
+            let x2 = rng.gen_range(x1..(x1+max_size).min(gridsize));
+            let y2 = rng.gen_range(y1..(y1+max_size).min(gridsize));
+            let test_item = TestItem {
+                id: i,
+                pos: BB::new(x1,y1,x2,y2)
+            };
+            g.insert(test_item);
+        }
+        println!("Tree nodes: {}",g.tree.len());
+
+        let mut rng = thread_rng();
+        {
+            let x1 = rng.gen_range(0..gridsize);
+            let y1 = rng.gen_range(0..gridsize);
+            let x2 = rng.gen_range(x1..(x1+max_size).min(gridsize));
+            let y2 = rng.gen_range(y1..(y1+max_size).min(gridsize));
+
+            let pos = BB::new(x1,y1,x2,y2);
+            b.iter(||{
+                g.query_fn(pos, |item|{
+                    black_box(item);
+                });
+            });
+
+        }
+
     }
 }
