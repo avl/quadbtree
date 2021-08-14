768c8174e32a1ec7abb417bea57919254fd91cae
Anders Musikka <anders@andersmusikka.se>
2021-08-14 17:44:28 +0200

    Add Savefile-support

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,9 @@
 #![feature(bench_black_box)]
 #![feature(test)]
 
+#[macro_use]
+extern crate savefile_derive;
+
 use std::ops::{Sub, Add};
 use smallvec::smallvec;
 use std::collections::HashMap;
@@ -16,9 +19,10 @@ use std::arch::x86_64::{__m128i, _mm_set1_epi8, _mm_cvtsi128_si64x, _mm_and_si12
 #[cfg(all(target_feature = "avx512vl", target_feature="avx512bw"))]
 use std::arch::x86_64::{_mm_broadcastb_epi8, _mm_maskz_broadcastb_epi8};
 use smallvec::SmallVec;
+use savefile::{WithSchema, Serialize, Deserialize, Introspect};
 
 /// A coordinate point in the 2D world
-#[derive(Clone,Copy,PartialEq,Debug,Eq,Hash)]
+#[derive(Clone,Copy,PartialEq,Debug,Eq,Hash,Savefile)]
 pub struct Pos {
     /// The x-coordinate
     pub x: i32,
@@ -30,7 +34,7 @@ pub struct Pos {
 /// both the top_left corner and the bottom right corner.
 /// An object within a bounding box is contained entirely within
 /// the box.
-#[derive(Clone,Copy,PartialEq,Debug,Eq,Hash)]
+#[derive(Clone,Copy,PartialEq,Debug,Eq,Hash,Savefile)]
 pub struct BB {
     /// The top left corner of the bounding box. The corner itself is considered part
     /// of the box.
@@ -145,7 +149,7 @@ pub trait TreeNodeItem {
     /// The type of a unique key identifying a specific item.
     /// If you don't need this, you can use the BB as the key,
     /// or even Self.
-    type Key : PartialEq+Eq+Hash;
+    type Key : PartialEq+Eq+Hash+WithSchema+Serialize+Deserialize+Introspect;
 
     /// Return bounding box for the given item
     fn get_bb(&self) -> BB;
@@ -163,7 +167,8 @@ fn get_set_bits_below(bitmap:u64, bit_index: u64) -> u32 {
     (bitmap & below_mask).count_ones()
 }
 
-struct TreeNode<T> {
+#[derive(Savefile)]
+struct TreeNode<T:WithSchema + Serialize + Deserialize+Introspect> {
     bb: BB,
     sub_cell_size: i32,
     sub_cell_shift: u32,
@@ -197,7 +202,8 @@ struct TreeNode<T> {
 /// do not each cover large areas. The function which returns overlapping items
 /// will have bad performance if the number of overlaps is too large (say more than
 /// a few on average per item).
-pub struct QuadBTree<T:TreeNodeItem> {
+#[derive(Savefile)]
+pub struct QuadBTree<T:TreeNodeItem + WithSchema + Serialize + Deserialize+Introspect> {
     /// Each position in this list is a node_index.
     tree: Vec<TreeNode<T>>, //The first position is always the root
     first_free: Option<u32>, //If there are free nodes, this is the first free node index.
@@ -206,7 +212,9 @@ pub struct QuadBTree<T:TreeNodeItem> {
 
 
 
-impl<T:TreeNodeItem> QuadBTree<T> {
+
+
+impl<T:TreeNodeItem + WithSchema + Serialize + Deserialize+Introspect> QuadBTree<T> {
 
     /// Create a new QuadBTree of the given size.
     /// Only coordinates in the range 0..size are allowed
@@ -670,16 +678,17 @@ mod tests {
     use std::collections::{HashSet};
     use test::Bencher;
     use std::hint::black_box;
+    use savefile::{WithSchema, Serialize, Deserialize, Introspect};
 
     extern crate test;
 
-    pub(crate) fn for_test_get_first_free<T:TreeNodeItem>(tree:&QuadBTree<T>) -> Option<u32> {
+    pub(crate) fn for_test_get_first_free<T:TreeNodeItem+WithSchema+Serialize+Deserialize+Introspect>(tree:&QuadBTree<T>) -> Option<u32> {
         tree.first_free
     }
 
 
 
-    #[derive(Clone,Copy,PartialEq,Debug,Eq,Hash)]
+    #[derive(Clone,Copy,PartialEq,Debug,Eq,Hash,Savefile)]
     pub struct TestItem {
         id: u32,
         pos: BB,
